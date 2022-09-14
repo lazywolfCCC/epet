75b61c633aa77801e3c1ff93b4baaa2c58057638
lazywolfCCC <2505517604@qq.com>
2022-09-14 22:40:49 +0800

    add gitignore

--- target/classes/tree/moe/epet/entity/Delayed.java
+++
@@ -1,7 +0,0 @@
-package tree.moe.epet.entity;
-
-public interface Delayed<T> {
-	boolean addToDelayQueue(Delayed<T> Delayed);
-	boolean addToDelayQueue(T data);
-	void removeToOrderDelayQueue(T data);
-}
